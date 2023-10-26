d7dd297a19043b95889813163cbbd38b8f4a31a6
Wyatt May <wyattmay79@gmail.com>
2023-10-25 22:16:34 -0600

    Added boilerplate HTML, Angular materials for the HTML, updated app.module.ts with all Angular Materials

--- full-sail-frontend/src/app/app.module.ts
+++ full-sail-frontend/src/app/app.module.ts
@@ -7,18 +7,36 @@ import { HomeModule } from './home/home.module';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AccountComponent } from './components/account/account.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     AccountComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
