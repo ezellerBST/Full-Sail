32f36755007678cb175e3ef80099f03c306e7cae
Eric Zeller <ez@Erics-MacBook-Air.local>
2023-11-07 21:13:34 -0700

    start addTransactions

--- full-sail-frontend/src/app/components/account/account.component.ts
+++ full-sail-frontend/src/app/components/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { Firestore, addDoc, doc, setDoc, getDoc, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-account',
@@ -7,11 +8,11 @@ import { Auth } from '@angular/fire/auth';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-  
+
   user: any;
   displayName: string = "";
 
-  constructor(private auth: Auth) {}
+  constructor(private auth: Auth, private firestore: Firestore) { }
 
   ngOnInit(): void {
     this.getUserDetails();
@@ -28,5 +29,39 @@ export class AccountComponent implements OnInit {
       console.log(error);
     }
   }
-  
+
+  addTransactions() {
+    // Assume you have an array of transactions
+    const sampleTransactions = [
+      {
+        date: '2023-11-01',
+        description: 'Salary',
+        amount: 5000
+      },
+      {
+        date: '2023-11-02',
+        description: 'Groceries',
+        amount: -250
+      }
+    ];
+
+    // Iterating through the sample transactions and adding them to Firestore
+    sampleTransactions.forEach(async transaction => {
+      try {
+        const docRef = await addDoc(collection(this.firestore, 'transactions'), {
+          date: transaction.date,
+          description: transaction.description,
+          amount: transaction.amount
+        });
+
+        // Fetch the transaction data after the document is added
+        const docSnapshot = await getDoc(docRef);
+        if (docSnapshot.exists()) {
+          console.log('Transaction data:', docSnapshot.data());
+        }
+      } catch (error) {
+        console.error('Error: ', error);
+      }
+    });
+  }
 }
